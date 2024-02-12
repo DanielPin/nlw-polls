f293dc3c7e233104ebca1bf56671f6b082607eac
Daniel <daniel.pin@outlook.com>
2024-02-11 21:45:35 -0300

    Aula 03

--- src/http/server.ts
+++ src/http/server.ts
@@ -3,9 +3,12 @@ import { createPoll } from "./routes/create-poll";
 import { getPoll } from "./routes/get-poll";
 import { voteOnPoll } from "./routes/vote-on-poll";
 import cookie from "@fastify/cookie";
+import { register } from "module";
+import websocket from "@fastify/websocket";
+import { pollResults } from "./ws/poll-results";
 
 const app = fastify();
-
+app.register(websocket);
 app.register(cookie, {
   secret: "polls-app-nlw",
   hook: "onRequest",
@@ -14,6 +17,9 @@ app.register(cookie, {
 app.register(createPoll);
 app.register(getPoll);
 app.register(voteOnPoll);
+
+app.register(pollResults);
+
 
 app.listen({ port: 3000 }).then(() => {
   console.log("Http server running!");
